057bb98c09cd345ab3b57cd967635cbfa063f75d
tomox27 <turkialhassan7@gmail.com>
2026-02-06 00:58:49 -0500

    V1.0.7

--- index.tsx
+++ index.tsx
@@ -3,8 +3,11 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 
 const container = document.getElementById('root');
-
 if (container) {
-  const root = createRoot(container);
-  root.render(<App />);
+    const root = createRoot(container);
+    root.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
 }
